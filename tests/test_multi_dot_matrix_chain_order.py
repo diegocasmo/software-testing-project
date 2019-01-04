b0e46b7202e158e79d5f6a38c6d6a0b446ccfc1f
Simon <simon.pellgard@gmail.com>
2019-01-04 13:33:10 +0100

    Changed names and order of tests

--- tests/test_multi_dot_matrix_chain_order.py
+++ tests/test_multi_dot_matrix_chain_order.py
@@ -16,30 +16,55 @@ class TestMultiDotMatrixchainOrder(unittest.TestCase):
     Tests for the method _multi_dot_matrix_chain_order
     '''
 
-    def test_cost_matrix(self):
+    def test_zero_loops_order(self):
         '''
-        Test for testing the cost matrix, running all loops more than one time
-        prime paths [12,13,15,10,12] and [12,13,14,15,10,12]
+        Test for prime path [1,2,3,5] with array of length 1 and no cost return
         '''
-        arrays = [np.random.random((30, 35)),
-                  np.random.random((35, 15)),
-                  np.random.random((15, 5)),
-                  np.random.random((5, 10)),
-                  np.random.random((10, 20)),
-                  np.random.random((20, 25))]
-        m_expected = np.array([[0., 15750., 7875., 9375., 11875., 15125.],
-                               [0.,     0., 2625., 4375.,  7125., 10500.],
-                               [0.,     0.,    0.,  750.,  2500.,  5375.],
-                               [0.,     0.,    0.,    0.,  1000.,  3500.],
-                               [0.,     0.,    0.,    0.,     0.,  5000.],
-                               [0.,     0.,    0.,    0.,     0.,     0.]])
+        arrays = [np.random.random((30, 35))]
+        s_exp = np.array([0])
+
+        s = multi_dot_matrix_chain_order(arrays)
+        self.assertEqual(s,s_exp)
+
+    def test_zero_loops_cost(self):
+        '''
+        Test for prime path [1,2,3,4] with array of length 1 and cost matrix
+        '''
+        arrays = [np.random.random((30, 35))]
+        m_exp = np.array([0.0])
+
+        _, m = multi_dot_matrix_chain_order(arrays, return_costs=True)
+        self.assertEqual(m,m_exp)
+        
+    def test_one_loop_order(self):
+        '''
+        Test for running all loops only one time and not returning costs, 
+        prime path [12,13,14,15,10,11,7,8,2,3,5]
+        '''
+        arrays = [np.random.random((30, 30)),
+                  np.random.random((30, 30))]
+        s_expected = np.array([[0,0],[0,0]])
+        
+        s = multi_dot_matrix_chain_order(arrays)
+
+        assert_almost_equal(np.triu(s[:-1, 1:]),
+                            np.triu(s_expected[:-1, 1:]))
+
+    def test_one_loop_cost(self):
+        '''
+        Test for running all loops only one time and return cost matrix,
+        prime path [12,13,14,15,10,11,7,8,2,3,4]
+        '''
+        arrays = [np.random.random((30, 30)),
+                  np.random.random((30, 30))]
+        
+        m_expected = np.array([[0.,27000.],[0.,0.]])
         
         _, m = multi_dot_matrix_chain_order(arrays, return_costs=True)
 
-        # Only the upper triangular part (without the diagonal) is interesting.
         assert_almost_equal(np.triu(m), np.triu(m_expected))
 
-    def test_order_matrix(self):
+    def test_multiple_loops_order(self):
         '''
         Test for testing only the order, running all loops more than one time
         prime paths [12,13,15,10,12] and [12,13,14,15,10,12]
@@ -64,51 +89,25 @@ class TestMultiDotMatrixchainOrder(unittest.TestCase):
         assert_almost_equal(np.triu(s[:-1, 1:]),
                             np.triu(s_expected[:-1, 1:]))
     
-    def test_length_one_order(self):
+    def test_multiple_loops_cost(self):
         '''
-        Test for prime path [1,2,3,5] with array of length 1 and no cost return
+        Test for testing the cost matrix, running all loops more than one time
+        prime paths [12,13,15,10,12] and [12,13,14,15,10,12]
         '''
-        arrays = [np.random.random((30, 35))]
-        s_exp = np.array([0])
-
-        s = multi_dot_matrix_chain_order(arrays)
-        self.assertEqual(s,s_exp)
-
-    def test_length_one_cost_matrix(self):
-        '''
-        Test for prime path [1,2,3,4] with array of length 1 and cost matrix
-        '''
-        arrays = [np.random.random((30, 35))]
-        m_exp = np.array([0.0])
-
-        _, m = multi_dot_matrix_chain_order(arrays, return_costs=True)
-        self.assertEqual(m,m_exp)
-        
-
-    def test_length_two_order(self):
-        '''
-        Test for running all loops only one time and not returning costs, 
-        prime path [12,13,14,15,10,11,7,8,2,3,5]
-        '''
-        arrays = [np.random.random((30, 30)),
-                  np.random.random((30, 30))]
-        s_expected = np.array([[0,0],[0,0]])
-        
-        s = multi_dot_matrix_chain_order(arrays)
-
-        assert_almost_equal(np.triu(s[:-1, 1:]),
-                            np.triu(s_expected[:-1, 1:]))
-
-    def test_length_two_return_cost(self):
-        '''
-        Test for running all loops only one time and return cost matrix,
-        prime path [12,13,14,15,10,11,7,8,2,3,4]
-        '''
-        arrays = [np.random.random((30, 30)),
-                  np.random.random((30, 30))]
-        
-        m_expected = np.array([[0.,27000.],[0.,0.]])
+        arrays = [np.random.random((30, 35)),
+                  np.random.random((35, 15)),
+                  np.random.random((15, 5)),
+                  np.random.random((5, 10)),
+                  np.random.random((10, 20)),
+                  np.random.random((20, 25))]
+        m_expected = np.array([[0., 15750., 7875., 9375., 11875., 15125.],
+                               [0.,     0., 2625., 4375.,  7125., 10500.],
+                               [0.,     0.,    0.,  750.,  2500.,  5375.],
+                               [0.,     0.,    0.,    0.,  1000.,  3500.],
+                               [0.,     0.,    0.,    0.,     0.,  5000.],
+                               [0.,     0.,    0.,    0.,     0.,     0.]])
         
         _, m = multi_dot_matrix_chain_order(arrays, return_costs=True)
 
+        # Only the upper triangular part (without the diagonal) is interesting.
         assert_almost_equal(np.triu(m), np.triu(m_expected))
